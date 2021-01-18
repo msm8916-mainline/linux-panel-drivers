0d2fc19b853a976ae877cd73b88d51b897d32fdb
Minecrell <minecrell@minecrell.net>
2021-01-18 22:32:50 +0100

    config/lenovo-a6010: Only generate yassy-ili9881 using --dumb-dcs
    tianma-hx8394d seems to have proper MIPI DSI DCS commands.

--- config/lenovo-a6010.dumb.sh
+++ config/lenovo-a6010.dumb.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # DTB: From Lenovo_A6010_S025_Q113507_20160408.zip
 
-OPTIONS=(-r vsp -r vsn)
+OPTIONS=(-r vsp -r vsn --dumb-dcs)
 PANELS=(
-	[hx8394d_720p_video]="wingtech,tianma-hx8394d"
+	[ili9881_720p_video]="wingtech,yassy-ili9881"
 )
