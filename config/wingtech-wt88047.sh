a49a741ef1022fc59ca12bd7185d68804e048a78
Nikita Travkin <nikita@trvn.ru>
2021-08-08 13:55:44 +0500

    config/wingtech-wt88047: Add wingtech,boe-nt35521s panel driver

--- config/wingtech-wt88047.sh
+++ config/wingtech-wt88047.sh
@@ -5,6 +5,7 @@
 OPTIONS=(-r power)
 PANELS=(
 	[nt35521_720p_video]="wingtech,auo-nt35521"
+	[nt35521s_720p_video]="wingtech,boe-nt35521s"
 	[otm1285a_otp_720p_video]="wingtech,ebbg-otm1285a"
 	[r61308_720p_video]="wingtech,auo-r61308"
 	[r69431_720p_video]="wingtech,sharp-r69431"
