1ba2964475331daffabff2f82118895e6edf2be4
Siddharth Manthan <siddharth.manthan@gmail.com>
2022-02-23 14:12:54 -0500

    config/dtb: samsung-fortuna3g: Add s6d78a0-gh9607501a panel driver
    Signed-off-by: Siddharth Manthan <siddharth.manthan@gmail.com>

---
+++ config/samsung-fortuna3g.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# DTB: From G530HXXU2BOH7_G530HODD2BOH3_G530HXXU2BOH5_HOME.tar.md5 INS-G530HXXU2BOH7-20150909215514.zip
+
+OPTIONS=(-r vreg -r vdd)
+PANELS=(
+	[s6d78a0_gh9607501a_qhd]="samsung,s6d78a0-gh9607501a"
+)
