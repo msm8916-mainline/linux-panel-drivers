d3f3c8f514930fa1ddd391b6d30ac90e2cd758f6
Lin, Meng-Bo <linmengbo0689@protonmail.com>
2022-02-24 21:20:29 +0900

    config/dtb: samsung-fortunaltezt: Add hx8389c panel driver

---
+++ config/samsung-fortunaltezt.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# DTB: From AP_G530YZTS1API1_CL1076315_QB11020104_REV00_user_low_ship_MULTI_CERT.tar.md5
+OPTIONS=(-r vreg -r vdd)
+PANELS=(
+	[hx8389c_qhd]="samsung,hx8389c"
+)
