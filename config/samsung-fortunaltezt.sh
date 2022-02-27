f41772daafebeb916ffe26e405d4e8c4746f40c6
Nikita Travkin <nikita@trvn.ru>
2022-02-27 17:55:31 +0500

    config/dtb: samsung-fortunaltezt: fixup panel name suffix

--- config/samsung-fortunaltezt.sh
+++ config/samsung-fortunaltezt.sh
@@ -2,5 +2,5 @@
 # DTB: From AP_G530YZTS1API1_CL1076315_QB11020104_REV00_user_low_ship_MULTI_CERT.tar.md5
 OPTIONS=(-r vreg -r vdd)
 PANELS=(
-	[hx8389c_qhd]="samsung,hx8389c"
+	[hx8389c_qhd_video]="samsung,hx8389c"
 )
