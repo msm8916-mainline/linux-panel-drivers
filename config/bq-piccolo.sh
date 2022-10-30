24be69138edd0ed6b0e85b72ecfbfad26ab61f53
André Apitzsch <git@apitzsch.eu>
2022-10-30 19:09:39 +0100

    config/dtb: bq-piccolo: Add truly-nt35695 panel driver

---
+++ config/bq-piccolo.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# DTB: From https://dl.twrp.me/piccolo/twrp-3.7.0_9-0-piccolo.img
+
+OPTIONS=(-r power --dumb-dcs)
+PANELS=(
+	[truly_nt35695_1080p_cmd]="longcheer,truly-nt35695"
+)
