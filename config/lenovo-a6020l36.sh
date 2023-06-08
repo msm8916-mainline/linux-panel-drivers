64bfe3e6b3607a03abed0435e76e6e00bc3efaa2
RuizLenato <ruizlenato@protonmail.com>
2023-06-07 23:02:57 -0300

    config/dtb: lenovo-a6020l36: Add tm-otm1901a and nt35596 panels

---
+++ config/lenovo-a6020l36.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# DTB: Extracted from stock boot image
+
+OPTIONS=(-r vsp -r vsn)
+PANELS=(
+	[otm1901a_tm_1080p_video]="wingtech,tm-otm1901a"
+	[nt35596_1080p_video]="wingtech,nt35596"
+)
