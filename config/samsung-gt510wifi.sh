a19199996bb340867e24416f5edd9c9c455923b3
Minecrell <minecrell@minecrell.net>
2023-08-02 09:16:13 +0200

    Drop panel drivers for samsung-gt58/gt510
    These were upstreamed in the panel-samsung-s6d7aa0 driver. :tada:

--- config/samsung-gt510wifi.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# DTB: From T550XXU1CRG1_T550DBT1CRH1_HOME.tar.md5
-
-OPTIONS=(-r vmipi -r 5p4v)
-PANELS=(
-	[s6d7aa0_ltl101at01_wxga]="samsung,s6d7aa0-ltl101at01"
-)
--- config/samsung-gt58lte.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# DTB: From T355YDOU1CRB3_T355YODD1CRB2_T355YDOU1CRA1_HOME.tar.md5
-
-OPTIONS=(-r 5p4v -r vmipi)
-PANELS=(
-	[s6d7aa0_lsl080al03_wxga]="samsung,s6d7aa0-lsl080al03"
-)
