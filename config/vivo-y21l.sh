240007510c78178a8cb571dfce7edeeb64137971
raihan2000 <raihan1999ahamed@gmail.com>
2023-10-22 12:29:10 +0530

    config/dtb: vivo-y21l: Add orise8012a-tmfwvga panel

---
+++ config/vivo-y21l.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# DTB: From https://asia-sysupwsdl.vivoglobal.com/upgrade/official/officialFiles/PD1309F_EX_A_1.17.2-update-full_1546069741.zip
+
+OPTIONS=(-r power)
+PANELS=(
+	[orise8012a_tmfwvga_cmd]="vivo,orise8012a-tmfwvga"
+)
