30810829713e11a40a45ddf4ceb1979b395fb27e
Adam Słaboń <asaillen@protonmail.com>
2024-02-24 11:44:37 +0100

    config/dtb: lenovo-a6020a40: Add wingtech,otm-ili9881c panel driver

---
+++ config/lenovo-a6020a40.dumb.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# DTB: From AospExtended-v7.2-A6020-20200513-2005-OFFICIAL.zip
+
+OPTIONS=(-r vsp -r vsn --dumb-dcs)
+PANELS=(
+	[ili9881c_720p_video]="wingtech,otm-ili9881c"
+)
