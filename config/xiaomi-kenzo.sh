d282592576d941548480c9d4be1e3f4499d7052b
Barnabás Czémán <trabarni@gmail.com>
2024-06-09 00:14:38 +0200

    config/dtb: Add Redmi Note 3 (kenzo)

---
+++ config/xiaomi-kenzo.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+OPTIONS=(-r vsn -r vsp)
+PANELS=(
+	[boe_nt35532_1080p_video]="xiaomi,kenzo-nt35532-boe"
+	[sharp_r63315_1080p_video]="xiaomi,kenzo-r63315-sharp"	
+	[tianma_nt35596_1080p_video]="xiaomi,kenzo-nt35596-tianma"	
+)
