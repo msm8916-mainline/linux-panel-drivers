1c1ff73c8ea6578c01c225ec14de69351bf137c1
Celeste Lucero <celele64@outlook.com>
2024-06-11 13:31:30 +0200

    config/dtb: samsung-heatqlte: Enable backlight
    A new patch enables backlight control for this device's panel.
    Re-enable backlight code stub generation for it to base on top of
    
    Link: https://github.com/msm8916-mainline/linux/pull/358
    Signed-off-by: Celeste Lucero <celele64@outlook.com>

--- config/samsung-heatqlte.sh
+++ config/samsung-heatqlte.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # DTB: From AP_G357FZXXS1AQA2_CL572060_QB12156321_REV00_user_low_ship_MULTI_CERT.tar
 
-OPTIONS=(-r vddio -r vdd --no-backlight)
+OPTIONS=(-r vddio -r vdd)
 PANELS=(
 	[samsung_wvga_video]="samsung,s6288a0"
 )
