6972ae02e93c837e3d6f96b49ea535eb967b192e
André Apitzsch <git@apitzsch.eu>
2025-06-08 18:12:31 +0200

    config/dtb: Add BQ Aquaris X5 Plus (gohan)

---
+++ config/longcheer-l9360.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+OPTIONS=(-r vsn -r vsp)
+PANELS=(
+	[booyi_hx8399b_1080p_command]="longcheer,l9360-hx8399b"
+	[booyi_hx8399c_1080p_video]="longcheer,l9360-hx8399c"
+	[truly_nt35695_1080p_command]="longcheer,l9360-nt35695"
+	[truly_otm1906a_1080p_command]="longcheer,l9360-otm1906a"
+	[truly_r63350_1080p_video]="longcheer,l9360-r63350"
+)
