f2e3548c880aadc3d30b67ddb0e04364061ecdfc
bzy-080408 <Bzy080408@outlook.com>
2025-08-07 17:05:15 +0800

    config/dtb: Add LeEco Le2 (s2)
    Signed-off-by: bzy-080408 <Bzy080408@outlook.com>

---
+++ config/leeco-s2.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# device-tree dump from official twrp 3.7.0
+
+OPTIONS=(-r vsn -r vsp)
+PANELS=(
+	[boe_nt35596s_fhd_5p5boe_vdo]="leeco,s2-nt35596s-boe"
+	[truly_r69006_5p5lg_1080p_cmd]="leeco,s2-r69006-lg"	
+	[boe_nt36760_fhd_5p5boe_cmd]="leeco,s2-nt36760-boe"	
+)
