788ed5c8efd1cafaec1e15007f69737e6086f536
Erikas Bitovtas <xerikasxx@gmail.com>
2025-09-23 01:21:22 +0300

    config/dtb: asus-z00t: add tm5p5-nt35596 panel driver

---
+++ config/asus-z00t.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# DTB: From TWRP-3.7.0_9-0-Z00T.img
+
+OPTIONS=(-r power --backlight-gpio --dcs-no-get-brightness)
+PANELS=(
+	[tm5p5_nt35596_1080p_video]="asus,z00t-tm5p5-nt35596"
+)
